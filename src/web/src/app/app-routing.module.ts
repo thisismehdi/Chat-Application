efe37c213072d38647f6c150daef054bd30dc116
thisismehdi <mehdifilalipro@gmail.com>
2024-12-02 14:17:15 +0100

    angular template

--- src/web/src/app/app-routing.module.ts
+++ src/web/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ChatComponent} from "./chat/chat.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChatComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
